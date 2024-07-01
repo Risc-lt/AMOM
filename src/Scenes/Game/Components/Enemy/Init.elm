7a0829c2bad047a44c874885ec1f1c782dafe2d8
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-07-01 23:21:31 +0800

    feat: add layer and two main comp

---
+++ src/Scenes/Game/Components/Enemy/Init.elm
@@ -0,0 +1,16 @@
+module Scenes.Game.Components.Enemy.Init exposing (InitData)
+
+{-|
+
+
+# Init module
+
+@docs InitData
+
+-}
+
+
+{-| The data used to initialize the scene
+-}
+type alias InitData =
+    {}
---
+++ src/Scenes/Game/Play/Init.elm
@@ -0,0 +1,16 @@
+module Scenes.Game.Play.Init exposing (InitData)
+
+{-|
+
+
+# Init module
+
+@docs InitData
+
+-}
+
+
+{-| The data used to initialize the scene
+-}
+type alias InitData =
+    {}

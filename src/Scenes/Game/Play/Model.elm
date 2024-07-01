69a9fb2b3ae84e99ebd9d9db90b2ce00a7e1bd13
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-07-01 23:21:31 +0800

    feat: add layer and two main comp

---
+++ src/Scenes/Game/Play/Model.elm
@@ -0,0 +1,62 @@
+module Scenes.Game.Play.Model exposing (layer)
+
+{-| Layer configuration module
+
+Set the Data Type, Init logic, Update logic, View logic and Matcher logic here.
+
+@docs layer
+
+-}
+
+import Canvas
+import Lib.Base exposing (SceneMsg)
+import Lib.UserData exposing (UserData)
+import Messenger.GeneralModel exposing (Matcher)
+import Messenger.Layer.Layer exposing (ConcreteLayer, LayerInit, LayerStorage, LayerUpdate, LayerUpdateRec, LayerView, genLayer)
+import Scenes.Game.SceneBase exposing (..)
+
+
+type alias Data =
+    {}
+
+
+init : LayerInit SceneCommonData UserData LayerMsg Data
+init env initMsg =
+    {}
+
+
+update : LayerUpdate SceneCommonData UserData LayerTarget LayerMsg SceneMsg Data
+update env evt data =
+    ( data, [], ( env, False ) )
+
+
+updaterec : LayerUpdateRec SceneCommonData UserData LayerTarget LayerMsg SceneMsg Data
+updaterec env msg data =
+    ( data, [], env )
+
+
+view : LayerView SceneCommonData UserData Data
+view env data =
+    Canvas.empty
+
+
+matcher : Matcher Data LayerTarget
+matcher data tar =
+    tar == "Play"
+
+
+layercon : ConcreteLayer Data SceneCommonData UserData LayerTarget LayerMsg SceneMsg
+layercon =
+    { init = init
+    , update = update
+    , updaterec = updaterec
+    , view = view
+    , matcher = matcher
+    }
+
+
+{-| Layer generator
+-}
+layer : LayerStorage SceneCommonData UserData LayerTarget LayerMsg SceneMsg
+layer =
+    genLayer layercon

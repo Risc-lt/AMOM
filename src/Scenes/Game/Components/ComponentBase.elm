128a354e53fc630542dd7381cf8c864e5e2ec123
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-07-02 16:08:01 +0800

    fix: add Gamestate

--- src/Scenes/Game/Components/ComponentBase.elm
+++ src/Scenes/Game/Components/ComponentBase.elm
@@ -1,4 +1,4 @@
-module Scenes.Game.Components.ComponentBase exposing (BaseData, ComponentMsg(..), ComponentTarget)
+module Scenes.Game.Components.ComponentBase exposing (BaseData, ComponentMsg(..), ComponentTarget, Gamestate(..))
 
 import Scenes.Game.Components.Enemy.Init exposing (Enemy)
 import Scenes.Game.Components.Self.Init exposing (Self)
@@ -12,13 +12,13 @@ import Scenes.Game.Components.Self.Init exposing (Self)
 @docs ComponentMsg: Component message
 @docs ComponentTarget: Component target
 @docs BaseData: Component base data
+@docs GamState
 
 -}
 type ComponentMsg
     = EnemyInit Enemy
     | SelfInit Self
     | PhysicalAttack Int
-    | PhysicalHurt Int
     | ReturnPlace
     | NullComponentMsg
 
@@ -27,5 +27,15 @@ type alias ComponentTarget =
     String
 
 
+type Gamestate
+    = GameBegin
+    | PlayerTurn
+    | EnemyMove
+    | EnemyAttack
+    | EnemyReturn
+    | GameOver
+
+
 type alias BaseData =
-    ()
+    { state : Gamestate
+    }

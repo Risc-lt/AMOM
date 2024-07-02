2e6cae892e8b5b279164dfbb788a80209a080533
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-07-02 16:08:01 +0800

    fix: add Gamestate

--- src/Scenes/Game/Components/Enemy/Model.elm
+++ src/Scenes/Game/Components/Enemy/Model.elm
@@ -9,9 +9,11 @@ module Scenes.Game.Components.Enemy.Model exposing (component)
 import Canvas
 import Lib.Base exposing (SceneMsg)
 import Lib.UserData exposing (UserData)
+import Messenger.Base exposing (UserEvent(..))
 import Messenger.Component.Component exposing (ComponentInit, ComponentMatcher, ComponentStorage, ComponentUpdate, ComponentUpdateRec, ComponentView, ConcreteUserComponent, genComponent)
+import Messenger.GeneralModel exposing (Msg(..))
 import Messenger.Render.Sprite exposing (renderSprite)
-import Scenes.Game.Components.ComponentBase exposing (BaseData, ComponentMsg(..), ComponentTarget)
+import Scenes.Game.Components.ComponentBase exposing (BaseData, ComponentMsg(..), ComponentTarget, Gamestate(..))
 import Scenes.Game.Components.Enemy.Init exposing (Enemy)
 import Scenes.Game.SceneBase exposing (SceneCommonData)
 
@@ -24,20 +26,45 @@ init : ComponentInit SceneCommonData UserData ComponentMsg Data BaseData
 init env initMsg =
     case initMsg of
         EnemyInit initData ->
-            ( initData, () )
+            ( initData, { state = PlayerTurn } )
 
         _ ->
-            ( { x = 100, y = 100, hp = 100, id = 1 }, () )
+            ( { x = 100, y = 100, hp = 100, id = 1 }, { state = PlayerTurn } )
+
+
+attackPlayer : ComponentUpdate SceneCommonData Data UserData SceneMsg ComponentTarget ComponentMsg BaseData
+attackPlayer env evnt data basedata =
+    ( ( data, { basedata | state = PlayerTurn } ), [ Other ( "Self", PhysicalAttack 1 ) ], ( env, False ) )
 
 
 update : ComponentUpdate SceneCommonData Data UserData SceneMsg ComponentTarget ComponentMsg BaseData
 update env evnt data basedata =
-    ( ( data, basedata ), [], ( env, False ) )
+    case evnt of
+        Tick _ ->
+            if basedata.state == EnemyMove then
+                ( ( { data | x = 400 }, { basedata | state = EnemyAttack } ), [], ( env, False ) )
+
+            else if basedata.state == EnemyAttack then
+                attackPlayer env evnt data basedata
+
+            else if basedata.state == PlayerTurn then
+                ( ( { data | x = 100 }, basedata ), [], ( env, False ) )
+
+            else
+                ( ( data, basedata ), [], ( env, False ) )
+
+        _ ->
+            ( ( data, basedata ), [], ( env, False ) )
 
 
 updaterec : ComponentUpdateRec SceneCommonData Data UserData SceneMsg ComponentTarget ComponentMsg BaseData
 updaterec env msg data basedata =
-    ( ( data, basedata ), [], env )
+    case msg of
+        PhysicalAttack id ->
+            ( ( { data | hp = data.hp - 10 }, basedata ), [], env )
+
+        _ ->
+            ( ( data, basedata ), [], env )
 
 
 view : ComponentView SceneCommonData UserData Data BaseData
--- src/Scenes/Game/Components/Self/Model.elm
+++ src/Scenes/Game/Components/Self/Model.elm
@@ -13,7 +13,7 @@ import Messenger.Base exposing (UserEvent(..))
 import Messenger.Component.Component exposing (ComponentInit, ComponentMatcher, ComponentStorage, ComponentUpdate, ComponentUpdateRec, ComponentView, ConcreteUserComponent, genComponent)
 import Messenger.GeneralModel exposing (Msg(..))
 import Messenger.Render.Sprite exposing (renderSprite)
-import Scenes.Game.Components.ComponentBase exposing (BaseData, ComponentMsg(..), ComponentTarget)
+import Scenes.Game.Components.ComponentBase exposing (BaseData, ComponentMsg(..), ComponentTarget, Gamestate(..))
 import Scenes.Game.Components.Self.Init exposing (Self)
 import Scenes.Game.SceneBase exposing (SceneCommonData)
 
@@ -26,28 +26,41 @@ init : ComponentInit SceneCommonData UserData ComponentMsg Data BaseData
 init env initMsg =
     case initMsg of
         SelfInit initData ->
-            ( initData, () )
+            ( initData, { state = PlayerTurn } )
 
         _ ->
-            ( { x = 800, y = 100, hp = 100, id = 1 }, () )
+            ( { x = 800, y = 100, hp = 100, id = 1 }, { state = PlayerTurn } )
 
 
 handleKeyDown : Int -> ComponentUpdate SceneCommonData Data UserData SceneMsg ComponentTarget ComponentMsg BaseData
 handleKeyDown key env evnt data basedata =
     case key of
         13 ->
-            -- ( ( { data | x = data.x - 400 }, () ), [ Other ( "Enemy", PhysicalAttack 1 ), Other ( "Self", ReturnPlace ) ], ( env, False ) )
-            ( ( { data | x = data.x - 400 }, () ), [], ( env, False ) )
+            ( ( data, { basedata | state = EnemyMove } ), [ Other ( "Enemy", PhysicalAttack 1 ) ], ( env, False ) )
 
         _ ->
-            ( ( { x = 800, y = 100, hp = 100, id = 1 }, () ), [], ( env, False ) )
+            ( ( data, basedata ), [], ( env, False ) )
 
 
 update : ComponentUpdate SceneCommonData Data UserData SceneMsg ComponentTarget ComponentMsg BaseData
 update env evnt data basedata =
     case evnt of
+        Tick _ ->
+            if basedata.state == PlayerTurn then
+                ( ( { data | x = 400 }, basedata ), [], ( env, False ) )
+
+            else if basedata.state == EnemyMove then
+                ( ( { data | x = 800 }, basedata ), [], ( env, False ) )
+
+            else
+                ( ( data, basedata ), [], ( env, False ) )
+
         KeyDown key ->
-            handleKeyDown key env evnt data basedata
+            if basedata.state == PlayerTurn then
+                handleKeyDown key env evnt data basedata
+
+            else
+                ( ( data, basedata ), [], ( env, False ) )
 
         _ ->
             ( ( data, basedata ), [], ( env, False ) )
@@ -56,8 +69,9 @@ update env evnt data basedata =
 updaterec : ComponentUpdateRec SceneCommonData Data UserData SceneMsg ComponentTarget ComponentMsg BaseData
 updaterec env msg data basedata =
     case msg of
-        -- ReturnPlace ->
-        --     ( ( { data | x = 800 }, basedata ), [], env )
+        PhysicalAttack id ->
+            ( ( { data | hp = data.hp - 10 }, basedata ), [], env )
+
         _ ->
             ( ( data, basedata ), [], env )
 

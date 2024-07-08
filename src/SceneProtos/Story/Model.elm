151b39cf0f5c1218d4c60260354cb38c585229b5
Tian Xia <Summer_2420@sjtu.edu.cn>
2024-07-08 19:59:37 +0800

    feat: story proto initialize

---
+++ src/SceneProtos/Story/Model.elm
@@ -0,0 +1,48 @@
+module SceneProtos.Story.Model exposing (genScene)
+
+{-| Scene configuration module
+
+@docs genScene
+
+-}
+
+import Lib.Base exposing (SceneMsg)
+import Lib.UserData exposing (UserData)
+import Messenger.Base exposing (Env, addCommonData)
+import Messenger.Scene.LayeredScene exposing (LayeredSceneLevelInit, LayeredSceneProtoInit, LayeredSceneSettingsFunc, genLayeredScene, initCompose)
+import Messenger.Scene.Scene exposing (SceneStorage)
+import SceneProtos.Story.Init exposing (InitData)
+import SceneProtos.Story.SceneBase exposing (..)
+
+
+commonDataInit : Env () UserData -> Maybe InitData -> SceneCommonData
+commonDataInit _ _ =
+    {}
+
+
+init : LayeredSceneProtoInit SceneCommonData UserData LayerTarget LayerMsg SceneMsg InitData
+init env data =
+    let
+        cd =
+            commonDataInit env data
+
+        envcd =
+            addCommonData cd env
+    in
+    { renderSettings = []
+    , commonData = cd
+    , layers =
+        []
+    }
+
+
+settings : LayeredSceneSettingsFunc SceneCommonData UserData LayerTarget LayerMsg SceneMsg
+settings _ _ _ =
+    []
+
+
+{-| Scene generator
+-}
+genScene : LayeredSceneLevelInit UserData SceneMsg InitData -> SceneStorage UserData SceneMsg
+genScene initd =
+    genLayeredScene (initCompose init initd) settings

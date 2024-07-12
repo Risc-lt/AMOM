d730469585ed57a7f6ec7a49f2d7108bd967efb3
Wang Xuanhuan <bluesirons@sjtu.edu.dn>
2024-07-12 20:13:23 +0800

    fix: remove false background

--- src/SceneProtos/Story/Components/Wenderd/Model.elm
+++ src/SceneProtos/Story/Components/Wenderd/Model.elm
@@ -9,12 +9,11 @@ module SceneProtos.Story.Components.Wenderd.Model exposing (component)
 import Canvas
 import Lib.Base exposing (SceneMsg)
 import Lib.UserData exposing (UserData)
-import Messenger.Component.Component exposing (ComponentInit, ComponentMatcher, ComponentStorage, ComponentUpdate, ComponentUpdateRec, ComponentView, ConcreteUserComponent, genComponent)
-import SceneProtos.Story.Components.ComponentBase exposing (BaseData, ComponentMsg, ComponentTarget)
-import SceneProtos.Story.SceneBase exposing (SceneCommonData)
-import Messenger.Render.Sprite exposing (renderSprite)
 import Messenger.Base exposing (UserEvent(..))
-import SceneProtos.Story.Components.ComponentBase exposing (ComponentMsg(..))
+import Messenger.Component.Component exposing (ComponentInit, ComponentMatcher, ComponentStorage, ComponentUpdate, ComponentUpdateRec, ComponentView, ConcreteUserComponent, genComponent)
+import Messenger.Render.Sprite exposing (renderSprite)
+import SceneProtos.Story.Components.ComponentBase exposing (BaseData, ComponentMsg(..), ComponentTarget)
+import SceneProtos.Story.SceneBase exposing (SceneCommonData)
 
 
 type alias Data =
@@ -26,47 +25,66 @@ type alias Data =
     , visible : Bool
     , dx : Float
     , dy : Float
-    , destination : (Float,Float)
+    , destination : ( Float, Float )
     }
 
 
 init : ComponentInit SceneCommonData UserData ComponentMsg Data BaseData
 init env initMsg =
     ( { standingFigure = "swordsman"
-        , movingSheet = "swordsman"
-        , currentFrame = 1
-        , position = (0,0), move = False, visible = False, dx = 0, dy = 0, destination = (0,0)
-    }, () )
+      , movingSheet = "swordsman"
+      , currentFrame = 1
+      , position = ( 0, 0 )
+      , move = False
+      , visible = False
+      , dx = 0
+      , dy = 0
+      , destination = ( 0, 0 )
+      }
+    , ()
+    )
+
 
 update : ComponentUpdate SceneCommonData Data UserData SceneMsg ComponentTarget ComponentMsg BaseData
 update env evnt data basedata =
     let
         newPos =
             updatePos data.position data.dx data.dy
+
         newCurrentFrame =
-            (modBy 4 (data.currentFrame + 1)) + 1
-        newDestination = 
+            modBy 4 (data.currentFrame + 1) + 1
+
+        newDestination =
             if isReached data.position newPos data.destination then
                 newPos
+
             else
                 data.destination
+
         newMove =
             if isReached data.position newPos data.destination then
                 True
+
             else
                 False
     in
     case evnt of
-        Tick dt ->  
-            ( ( { data | position = newPos, currentFrame = newCurrentFrame , destination = newDestination, move = newMove }, basedata ), [], ( env, False ) )
+        Tick dt ->
+            ( ( { data | position = newPos, currentFrame = newCurrentFrame, destination = newDestination, move = newMove }, basedata ), [], ( env, False ) )
+
         _ ->
-            ( (  data, basedata ), [], ( env, False ) )
-            
+            ( ( data, basedata ), [], ( env, False ) )
+
+
+
 -- whether the Position is reach or over the destination
-isReached : (Float,Float) -> (Float,Float) -> (Float,Float) -> Bool
-isReached (originX,originY) (newX,newY) (desX,desY) =
-    if (originX - desX)*(newX - desX) < 0 ||(originX - desX)*(newX - desX) == 0 then
+
+
+isReached : ( Float, Float ) -> ( Float, Float ) -> ( Float, Float ) -> Bool
+isReached ( originX, originY ) ( newX, newY ) ( desX, desY ) =
+    if (originX - desX) * (newX - desX) < 0 || (originX - desX) * (newX - desX) == 0 then
         True
+
     else
         False
 
@@ -74,57 +92,83 @@ isReached (originX,originY) (newX,newY) (desX,desY) =
 updaterec : ComponentUpdateRec SceneCommonData Data UserData SceneMsg ComponentTarget ComponentMsg BaseData
 updaterec env msg data basedata =
     case msg of
-        NewWenderdMsg (x,y) ->
+        NewWenderdMsg ( x, y ) ->
             let
-                initPosition = (x,y)
+                initPosition =
+                    ( x, y )
             in
             ( ( { data | position = initPosition, visible = True }, basedata ), [], env )
-        VanishWenderdMsg -> 
+
+        VanishWenderdMsg ->
             ( ( { data | visible = False }, basedata ), [], env )
-        MoveTo (x,y) -> 
+
+        MoveTo ( x, y ) ->
             let
-                newDes = (x,y)
-                newDX = (x - data.dx)/5
-                newDY = (y - data.dy)/5
+                newDes =
+                    ( x, y )
+
+                newDX =
+                    (x - data.dx) / 5
+
+                newDY =
+                    (y - data.dy) / 5
             in
             ( ( { data | move = True, destination = newDes, dx = newDX, dy = newDY }, basedata ), [], env )
-        CameraMsg (x,y) ->
+
+        CameraMsg ( x, y ) ->
             let
-                newDes = (x,y)
-                newDX = (x - data.dx)/5
-                newDY = (y - data.dy)/5
+                newDes =
+                    ( x, y )
+
+                newDX =
+                    (x - data.dx) / 5
+
+                newDY =
+                    (y - data.dy) / 5
             in
             ( ( { data | move = True, destination = newDes, dx = newDX, dy = newDY }, basedata ), [], env )
+
         _ ->
             ( ( data, basedata ), [], env )
 
-updatePos : (Float,Float) -> Float -> Float -> (Float,Float)
-updatePos (x,y) dx dy =
-    let
-        newX = x+dx
-        newY = y+dy
-    in
-        ( newX, newY )
 
+updatePos : ( Float, Float ) -> Float -> Float -> ( Float, Float )
+updatePos ( x, y ) dx dy =
+    let
+        newX =
+            x + dx
+
+        newY =
+            y + dy
+    in
+    ( newX, newY )
 
 
 view : ComponentView SceneCommonData UserData Data BaseData
 view env data basedata =
     if data.visible == True then
         let
-            newSprite = 
+            newSprite =
                 if data.move then
                     data.movingSheet ++ "." ++ String.fromInt data.currentFrame
+
                 else
                     data.standingFigure
         in
-           ( Canvas.group 
-           [] 
-           [ renderSprite env.globalData.internalData [] data.position (150,0) newSprite
-           ]
-           , 54)
+        ( Canvas.group
+            []
+            [ renderSprite env.globalData.internalData [] data.position ( 150, 0 ) newSprite
+            ]
+        , 54
+        )
+
     else
-    ( Canvas.empty, 54 ) -- z-index
+        ( Canvas.empty, 54 )
+
+
+
+-- z-index
+
 
 matcher : ComponentMatcher Data BaseData ComponentTarget
 matcher data basedata tar =
--- src/SceneProtos/Story/Model.elm
+++ src/SceneProtos/Story/Model.elm
@@ -11,15 +11,15 @@ import Lib.UserData exposing (UserData)
 import Messenger.Base exposing (Env, addCommonData)
 import Messenger.Scene.LayeredScene exposing (LayeredSceneLevelInit, LayeredSceneProtoInit, LayeredSceneSettingsFunc, genLayeredScene, initCompose)
 import Messenger.Scene.Scene exposing (SceneStorage)
+import SceneProtos.Story.Components.Background.Model as Background
+import SceneProtos.Story.Components.Bithif.Model as Bithif
+import SceneProtos.Story.Components.Bruce.Model as Bruce
+import SceneProtos.Story.Components.Bulingze.Model as Bulingze
+import SceneProtos.Story.Components.Dialogue.Model as Dialogue
+import SceneProtos.Story.Components.Wenderd.Model as Wendered
 import SceneProtos.Story.Init exposing (InitData)
 import SceneProtos.Story.Main.Model as Main
 import SceneProtos.Story.SceneBase exposing (..)
-import SceneProtos.Story.Components.Background.Model as Background
-import SceneProtos.Story.Components.Bithif.Model as Bithif
-import SceneProtos.Story.Components.Dialogue.Model as Dialogue
-import SceneProtos.Story.Components.Bruce.Model as Bruce
-import SceneProtos.Story.Components.Bulingze.Model as Bulingze
-import SceneProtos.Story.Components.Wenderd.Model as Wendered
 
 
 commonDataInit : Env () UserData -> Maybe (InitData SceneMsg) -> SceneCommonData

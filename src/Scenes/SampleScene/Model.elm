e4fd2d40aa454ff506b300fc95d1f341622b3eeb
Tian Xia <Summer_2420@sjtu.edu.cn>
2024-07-18 14:50:18 +0800

    style: elm-formatted

--- src/Scenes/SampleScene/Model.elm
+++ src/Scenes/SampleScene/Model.elm
@@ -9,6 +9,7 @@ module Scenes.SampleScene.Model exposing (scene)
 
 import Lib.Base exposing (SceneMsg)
 import Lib.UserData exposing (UserData)
+import MainConfig exposing (background)
 import Messenger.Base exposing (Env)
 import Messenger.Scene.RawScene exposing (RawSceneProtoLevelInit)
 import Messenger.Scene.Scene exposing (SceneStorage)
@@ -16,24 +17,25 @@ import SceneProtos.Story.Components.Background.Model as Background
 import SceneProtos.Story.Components.Bithif.Model as Bithif
 import SceneProtos.Story.Components.Bruce.Model as Bruce
 import SceneProtos.Story.Components.Bulingze.Model as Bulingze
-import SceneProtos.Story.Components.Wenderd.Model as Wenderd
 import SceneProtos.Story.Components.ComponentBase exposing (ComponentMsg(..))
 import SceneProtos.Story.Components.Dialogue.Model as Dialogue
+import SceneProtos.Story.Components.Wenderd.Model as Wenderd
 import SceneProtos.Story.Init exposing (InitData)
 import SceneProtos.Story.Model exposing (genScene)
-import MainConfig exposing (background)
 
 
 init : RawSceneProtoLevelInit UserData SceneMsg (InitData SceneMsg)
 init env msg =
     Just (initData env msg)
 
+
 initData : Env () UserData -> Maybe SceneMsg -> InitData SceneMsg
-initData env msg = { objects =
-        [ 
-        ]
+initData env msg =
+    { objects =
+        []
     , order = "1"
     }
+
 
 {-| Scene storage
 -}

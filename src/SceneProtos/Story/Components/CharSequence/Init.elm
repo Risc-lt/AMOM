4a4f40589704d704ddb2f7f5d9d509856d94a407
Tian Xia <Summer_2420@sjtu.edu.cn>
2024-07-25 10:06:17 +0800

    feat: messenger init component

---
+++ src/SceneProtos/Story/Components/CharSequence/Init.elm
@@ -0,0 +1,16 @@
+module SceneProtos.Story.Components.CharSequence.Init exposing (InitData)
+
+{-|
+
+
+# Init module
+
+@docs InitData
+
+-}
+
+
+{-| The data used to initialize the scene
+-}
+type alias InitData =
+    {}

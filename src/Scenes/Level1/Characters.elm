9cb6ef15c5004936ecbc443b15859d38183db522
Wang Xuanhuan <bluesirons@sjtu.edu.dn>
2024-07-25 19:43:58 +0800

    feat: genSelf

--- src/Scenes/Level1/Characters.elm
+++ src/Scenes/Level1/Characters.elm
@@ -8,6 +8,7 @@ module Scenes.Level1.Characters exposing (..)
 
 import SceneProtos.Game.Components.Enemy.Init exposing (Enemy)
 import SceneProtos.Game.Components.Self.Init exposing (Self, defaultSelf)
+import SceneProtos.Game.Components.Special.Init exposing (Skill)
 import SceneProtos.Game.Components.Special.Library exposing (..)
 import SceneProtos.Game.Components.GenAttributes exposing (..)
 
@@ -29,14 +30,21 @@ wenderd time =
             , earthResistance = 10
             }
     in
-    { defaultSelf |
-    name = "Wenderd"
-    , x = 1100
-    , y = 160
-    , position = 1
+    genSelf 1 time "Wenderd" baseAttributes baseEleResistance
+        [ arcaneBeam
+        , airBlade
+        ]
+
+
+genSelf : Int -> Int -> String -> Attribute -> EleResistance -> List Skill -> Self
+genSelf position time name baseAttributes baseEleResistance skills =
+    { defaultSelf
+    | name = name
+    , x = if position <= 3 then 1100 else 1220
+    , y = toFloat (160 + 130 * (position - (position - 1) // 3 * 3 - 1))
+    , position = position
     , hp = genHp baseAttributes
     , mp = genMp baseAttributes
-    , energy = 0
     , attributes = baseAttributes
     , extendValues =
         genExtendValues
@@ -46,15 +54,8 @@ wenderd time =
             baseEleResistance.fireResistance
             baseEleResistance.airResistance
             baseEleResistance.earthResistance
-    , buff = []
-    , skills =
-        [ airBlade
-        , arcaneBeam
-        ]
+    , skills = skills
     }
-
-
-genSelf : String -> Attribute -> EleResistance -> 
 
 
 {-| Init data for selfs
@@ -63,4 +64,3 @@ selfInitData : Int -> List Self
 selfInitData time =
     [ wenderd time
     ]
-    

5f0f0f6a4bc4f9bf4d55a902a2700bec139e4a86
Wang Xuanhuan <bluesirons@sjtu.edu.dn>
2024-07-26 19:08:17 +0800

    fix: indent error

--- src/SceneProtos/Story/Components/CharSequence/UpdateHelper.elm
+++ src/SceneProtos/Story/Components/CharSequence/UpdateHelper.elm
@@ -3,59 +3,59 @@ module SceneProtos.Story.Components.CharSequence.UpdateHelper exposing (..)
 
 updateHelper : ComponentUpdate SceneCommonData Data UserData SceneMsg ComponentTarget ComponentMsg BaseData
 updateHelper env evnt data basedata =
-let
-    curPlots =
-        List.map 
-            (\m -> 
-                Tuple.pair m <|
-                    Maybe.withDefault defaultCharacter <|
+    let
+        curPlots =
+            List.map 
+                (\m -> 
+                    Tuple.pair m <|
+                        Maybe.withDefault defaultCharacter <|
+                            List.head <|
+                                List.filter (\c -> c.name == m.name) data.characters
+                )
+                <| List.filter (\m -> m.isMoving == True) data.curMove
+
+        newPlots =
+            List.map 
+                (\( m, c ) ->
+                    checkDestination <| handleMove ( m, c )
+                )
+                curPlots
+
+        newMoves =
+            List.map
+                (\m ->
+                    case
                         List.head <|
-                            List.filter (\c -> c.name == m.name) data.characters
-            )
-            <| List.filter (\m -> m.isMoving == True) data.curMove
+                            List.filter (\n -> n.name == m.name) <|
+                                List.map Tuple.first newPlots
+                    of
+                        Just movement ->
+                            movement
 
-    newPlots =
-        List.map 
-            (\( m, c ) ->
-                checkDestination <| handleMove ( m, c )
-            )
-            curPlots
+                        _ ->
+                            m
+                )
+                data.curMove
 
-    newMoves =
-        List.map
-            (\m ->
-                case
-                    List.head <|
-                        List.filter (\n -> n.name == m.name) <|
-                            List.map Tuple.first newPlots
-                of
-                    Just movement ->
-                        movement
+        newChars =
+            List.map
+                (\c ->
+                    case
+                        List.head <|
+                            List.filter (\n -> n.name == c.name) <|
+                                List.map Tuple.second newPlots
+                    of
+                        Just movement ->
+                            movement
 
-                    _ ->
-                        m
-            )
-            data.curMove
+                        _ ->
+                            c
+                )
+                data.characters
 
-    newChars =
-        List.map
-            (\c ->
-                case
-                    List.head <|
-                        List.filter (\n -> n.name == c.name) <|
-                            List.map Tuple.second newPlots
-                of
-                    Just movement ->
-                        movement
-
-                    _ ->
-                        c
-            )
-            data.characters
-
-    newState =
-        List.any (\m -> m.isMoving == True) newMoves
-in
-( ( { data | characters = newChars, curMove = newMoves }, { basedata | isPlaying = not newState } )
-, []
-, ( env, False ) )
+        newState =
+            List.any (\m -> m.isMoving == True) newMoves
+    in
+    ( ( { data | characters = newChars, curMove = newMoves }, { basedata | isPlaying = not newState } )
+    , []
+    , ( env, False ) )

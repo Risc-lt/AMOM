b411d3ad3e28547c3cf113a0c4ff180ca74f67e3
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-07-27 19:46:37 +0800

    feat: add picking scroll in home page

--- src/Scenes/Home/Init.elm
+++ src/Scenes/Home/Init.elm
@@ -1,4 +1,10 @@
-module Scenes.Home.Init exposing (InitData)
+module Scenes.Home.Init exposing (Data, Direction(..), ScenePic, getX, initData)
+
+import Messenger.Audio.Base exposing (AudioOption(..))
+import Messenger.Base exposing (UserEvent(..))
+import Messenger.Scene.Scene exposing (SceneOutputMsg(..))
+import SceneProtos.Story.Components.CharSequence.Init exposing (Direction)
+
 
 {-|
 
@@ -8,9 +14,108 @@ module Scenes.Home.Init exposing (InitData)
 @docs InitData
 
 -}
+type alias Data =
+    { curScene : Float
+    , sceneQueue : List ScenePic
+    , direction : Direction
+    , left : Float
+    }
 
 
-{-| The data used to initialize the scene
--}
-type alias InitData =
-    {}
+type Direction
+    = Left
+    | Right
+    | Null
+
+
+type alias ScenePic =
+    { name : String
+    , x : Float
+    , y : Float
+    , w : Float
+    , h : Float
+    , id : Float
+    }
+
+
+initData : Data
+initData =
+    { curScene = 1
+    , sceneQueue =
+        [ { name = "dialogue_1"
+          , x = 720
+          , y = 300
+          , w = 500
+          , h = 400
+          , id = 1
+          }
+        , { name = "background"
+          , x = 1320
+          , y = 300
+          , w = 500
+          , h = 400
+          , id = 2
+          }
+        , { name = "dialogue_1"
+          , x = 1920
+          , y = 300
+          , w = 500
+          , h = 400
+          , id = 3
+          }
+        , { name = "dialogue_2"
+          , x = 2520
+          , y = 300
+          , w = 500
+          , h = 400
+          , id = 4
+          }
+        , { name = "background"
+          , x = 3120
+          , y = 300
+          , w = 500
+          , h = 400
+          , id = 5
+          }
+        , { name = "dialogue_2"
+          , x = 3720
+          , y = 300
+          , w = 500
+          , h = 400
+          , id = 6
+          }
+        , { name = "dialogue_3"
+          , x = 4320
+          , y = 300
+          , w = 500
+          , h = 400
+          , id = 7
+          }
+        , { name = "background"
+          , x = 4920
+          , y = 300
+          , w = 500
+          , h = 400
+          , id = 8
+          }
+        , { name = "dialogue_3"
+          , x = 5520
+          , y = 300
+          , w = 500
+          , h = 400
+          , id = 9
+          }
+        ]
+    , direction = Null
+    , left = 0
+    }
+
+
+getX : Float -> List ScenePic -> Float
+getX id sceneQueue =
+    case List.head (List.filter (\scenePic -> scenePic.id == id) sceneQueue) of
+        Just scenePic ->
+            scenePic.x
+
+        Nothing ->
+            0
--- src/Scenes/Home/Model.elm
+++ src/Scenes/Home/Model.elm
@@ -7,6 +7,8 @@ module Scenes.Home.Model exposing (scene)
 -}
 
 import Canvas
+import Canvas.Settings exposing (fill)
+import Color exposing (Color)
 import Duration
 import Lib.Base exposing (SceneMsg)
 import Lib.UserData exposing (UserData)
@@ -14,18 +16,18 @@ import Messenger.Audio.Base exposing (AudioOption(..))
 import Messenger.Base exposing (UserEvent(..))
 import Messenger.GlobalComponents.Transition.Transitions.Base exposing (genTransition)
 import Messenger.GlobalComponents.Transition.Transitions.Fade exposing (fadeInBlack, fadeOutBlack)
+import Messenger.Render.Shape exposing (rect)
 import Messenger.Render.Sprite exposing (renderSprite)
 import Messenger.Scene.RawScene exposing (RawSceneInit, RawSceneUpdate, RawSceneView, genRawScene)
 import Messenger.Scene.Scene exposing (MConcreteScene, SceneOutputMsg(..), SceneStorage)
-
-
-type alias Data =
-    {}
+import Scenes.Begin.Model exposing (scene)
+import Scenes.Home.Init exposing (Data, Direction(..), getX, initData)
+import String exposing (right)
 
 
 init : RawSceneInit Data UserData SceneMsg
 init env msg =
-    {}
+    initData
 
 
 update : RawSceneUpdate Data UserData SceneMsg
@@ -35,8 +37,41 @@ update env msg data =
             if x > 850 && x < 1060 && y > 430 && y < 580 then
                 ( data, [ SOMChangeScene Nothing "Level1" ], env )
 
+            else if x > 1800 && x < 1900 && y > 1000 && y < 1100 then
+                if data.curScene < 9 then
+                    ( { data | direction = Right }, [], env )
+
+                else
+                    ( data, [], env )
+
+            else if x > 0 && x < 100 && y > 1000 && y < 1100 then
+                if data.curScene > 1 then
+                    ( { data | direction = Left }, [], env )
+
+                else
+                    ( data, [], env )
+
             else
                 ( data, [], env )
+
+        Tick _ ->
+            case data.direction of
+                Right ->
+                    if data.left >= getX (data.curScene + 1) data.sceneQueue - 720 then
+                        ( { data | direction = Null, curScene = data.curScene + 1 }, [], env )
+
+                    else
+                        ( { data | left = data.left + 100 }, [], env )
+
+                Left ->
+                    if data.left <= getX (data.curScene - 1) data.sceneQueue - 720 then
+                        ( { data | direction = Null, curScene = data.curScene - 1 }, [], env )
+
+                    else
+                        ( { data | left = data.left - 100 }, [], env )
+
+                Null ->
+                    ( data, [], env )
 
         _ ->
             ( data, [], env )
@@ -44,9 +79,30 @@ update env msg data =
 
 view : RawSceneView UserData Data
 view env data =
+    let
+        basicView =
+            [ renderSprite env.globalData.internalData [] ( 0, 0 ) ( 1920, 1080 ) "background"
+            , Canvas.shapes
+                [ fill (Color.rgba 0 0 0 0.7) ]
+                [ rect env.globalData.internalData ( 1800, 1000 ) ( 100, 100 ) ]
+            , Canvas.shapes
+                [ fill (Color.rgba 0 0 0 0.7) ]
+                [ rect env.globalData.internalData ( 0, 1000 ) ( 100, 100 ) ]
+            ]
+
+        sceneView =
+            List.map
+                (\scenePic ->
+                    if scenePic.id == data.curScene then
+                        renderSprite env.globalData.internalData [] ( scenePic.x - data.left, scenePic.y ) ( scenePic.w, scenePic.h ) scenePic.name
+
+                    else
+                        renderSprite env.globalData.internalData [] ( scenePic.x - data.left, scenePic.y ) ( scenePic.w, scenePic.h ) scenePic.name
+                )
+                data.sceneQueue
+    in
     Canvas.group []
-        [ renderSprite env.globalData.internalData [] ( 0, 0 ) ( 1920, 1080 ) "instruction"
-        ]
+        (basicView ++ sceneView)
 
 
 scenecon : MConcreteScene Data UserData SceneMsg

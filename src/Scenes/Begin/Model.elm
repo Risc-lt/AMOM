0fd0bcd0836982e02e78dfa3f8d837b2ba8f7a11
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-07-27 16:27:55 +0800

    feat: add transition

--- src/Scenes/Begin/Model.elm
+++ src/Scenes/Begin/Model.elm
@@ -1,4 +1,4 @@
-module Scenes.Begin.Model exposing (scene)
+module Scenes.Begin.Model exposing (..)
 
 {-| Scene configuration module
 
@@ -6,42 +6,53 @@ module Scenes.Begin.Model exposing (scene)
 
 -}
 
+import Canvas
+import Duration
 import Lib.Base exposing (SceneMsg)
 import Lib.UserData exposing (UserData)
-import Messenger.Base exposing (Env, addCommonData)
-import Messenger.Scene.LayeredScene exposing (LayeredSceneInit, LayeredSceneSettingsFunc, genLayeredScene)
-import Messenger.Scene.Scene exposing (SceneStorage)
-import Scenes.Begin.SceneBase exposing (..)
+import Messenger.Audio.Base exposing (AudioOption(..))
+import Messenger.GlobalComponents.Transition.Transitions.Base exposing (genTransition)
+import Messenger.GlobalComponents.Transition.Transitions.Fade exposing (fadeInBlack, fadeOutBlack)
+import Messenger.Render.Sprite exposing (renderSprite)
+import Messenger.Scene.RawScene exposing (RawSceneInit, RawSceneUpdate, RawSceneView, genRawScene)
+import Messenger.Scene.Scene exposing (MConcreteScene, SceneOutputMsg(..), SceneStorage)
 
 
-commonDataInit : Env () UserData -> Maybe SceneMsg -> SceneCommonData
-commonDataInit _ _ =
+type alias Data =
     {}
 
 
-init : LayeredSceneInit SceneCommonData UserData LayerTarget LayerMsg SceneMsg
+init : RawSceneInit Data UserData SceneMsg
 init env msg =
-    let
-        cd =
-            commonDataInit env msg
+    {}
 
-        envcd =
-            addCommonData cd env
-    in
-    { renderSettings = []
-    , commonData = cd
-    , layers =
-        []
+
+update : RawSceneUpdate Data UserData SceneMsg
+update env msg data =
+    if env.globalData.sceneStartFrame == 125 then
+        ( data, [ SOMChangeScene Nothing "Level1" ], env )
+
+    else
+        ( data, [], env )
+
+
+view : RawSceneView UserData Data
+view env data =
+    Canvas.group []
+        [ renderSprite env.globalData.internalData [] ( 0, 0 ) ( 1920, 1080 ) "background"
+        ]
+
+
+scenecon : MConcreteScene Data UserData SceneMsg
+scenecon =
+    { init = init
+    , update = update
+    , view = view
     }
-
-
-settings : LayeredSceneSettingsFunc SceneCommonData UserData LayerTarget LayerMsg SceneMsg
-settings _ _ _ =
-    []
 
 
 {-| Scene generator
 -}
 scene : SceneStorage UserData SceneMsg
 scene =
-    genLayeredScene init settings
+    genRawScene scenecon

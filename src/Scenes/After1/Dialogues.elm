c6405b8f4eb84a7c801f6337dd105e6bfa927485
Wang Xuanhuan <bluesirons@sjtu.edu.dn>
2024-07-28 23:47:34 +0800

    feat: after1

--- src/Scenes/After1/Dialogues.elm
+++ src/Scenes/After1/Dialogues.elm
@@ -64,7 +64,84 @@ checkWolf =
     genDialogue "head_wenderd" content ( 4, 3 )
 
 
+remind : Dialogue
+remind =
+    let
+        content =
+            [ "Bulingze:"
+            , "Let go of that idea. All these furs must be handed over to the"
+            , "guild to prove our work results."
+            ]
+    in
+    genDialogue "head_bulingze" content ( 4, 4 )
 
+
+explain : Dialogue
+explain =
+    let
+        content =
+            [ "Wenderd:"
+            , "Okay, okay, I get it. You know, I'm just drooling over them."
+            , "Those people are just too greedy."
+            ]
+    in
+    genDialogue "head_wenderd" content ( 4, 5 )
+
+
+silence : Dialogue
+silence =
+    let
+        content =
+            [ "Wenderd:"
+            , "Well..."
+            ]
+    in
+    genDialogue "head_wenderd" content ( 4, 6 )
+
+
+recall : Dialogue
+recall =
+    let
+        content =
+            [ "Wenderd:"
+            , "Guess what, I kind of miss the barbecues we had in the woods"
+            , "before we came here."
+            ]
+    in
+    genDialogue "head_wenderd" content ( 4, 7 )
+
+
+sad : Dialogue
+sad =
+    let
+        content =
+            [ "Bithif:"
+            , "..."
+            ]
+    in
+    genDialogue "head_bithif" content ( 4, 8 )
+
+
+refuse : Dialogue
+refuse =
+    let
+        content =
+            [ "Bruce:"
+            , "...We can't. Not until my research is complete."
+            ]
+    in
+    genDialogue "head_bruce" content ( 4, 9 )
+
+
+end : Dialogue
+end =
+    let
+        content =
+            [ "Wenderd:"
+            , "I know, I know, so let's get this over with and get back."
+            ]
+    in
+    genDialogue "head_wenderd" content ( 4, 10 )
 
 
 genDialogue : String -> List String -> ( Int, Int ) -> Dialogue
@@ -85,5 +162,12 @@ dialogueInitData =
         , fear
         , agree
         , checkWolf
+        , remind
+        , explain
+        , silence
+        , recall
+        , sad
+        , refuse
+        , end
         ]
     }

a291b47827902911b57eb687b1192aaec476b783
Wang Xuanhuan <bluesirons@sjtu.edu.dn>
2024-07-28 11:39:05 +0800

    feat: before1 base

---
+++ src/Scenes/Before1/Characters.elm
@@ -0,0 +1,15 @@
+module Scenes.Before1.Characters exposing (..)
+
+{-
+    All charater data
+-}
+
+import SceneProtos.Story.Components.CharSequence.Init exposing (InitData)
+
+
+charInitData : InitData
+charInitData = 
+    { characters = []
+    , curMove = []
+    , remainMove = []
+    }

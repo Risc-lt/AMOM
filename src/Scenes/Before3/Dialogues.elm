8034ea276de9bf7d7bea3895bacca286b16de150
Wang Xuanhuan <bluesirons@sjtu.edu.dn>
2024-07-29 22:30:30 +0800

    feat: dialogues

---
+++ src/Scenes/Before3/Dialogues.elm
@@ -0,0 +1,25 @@
+module Scenes.Before3.Dialogues exposing (..)
+
+{-
+   All dialogue data
+-}
+
+import SceneProtos.Story.Components.DialogSequence.Init exposing (Dialogue, InitData, defaultDialogue)
+
+
+genDialogue : String -> List String -> ( Int, Int ) -> Dialogue
+genDialogue speaker content id =
+    { defaultDialogue
+        | speaker = speaker
+        , content = content
+        , id = id
+    }
+
+
+dialogueInitData : InitData
+dialogueInitData =
+    { curDialogue = defaultDialogue
+    , remainDiaList =
+        [ 
+        ]
+    }

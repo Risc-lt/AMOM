e9a738ea75092e3cb57c6b41d69fd0897e54646e
Wang Xuanhuan <bluesirons@sjtu.edu.dn>
2024-07-29 19:45:34 +0800

    feat: background

---
+++ src/Scenes/After2/Background.elm
@@ -0,0 +1,31 @@
+module Scenes.After2.Background exposing (..)
+
+{-
+   background data
+-}
+
+import SceneProtos.Story.Components.Background.Init exposing (Camera, InitData, defaultCamera)
+
+
+genCamera : Float -> Float -> Float -> Int -> Camera
+genCamera targetX targetY speed id =
+    { defaultCamera
+        | targetX = targetX
+        , targetY = targetY
+        , speed = speed
+        , isMoving = False
+        , id = id
+    }
+
+
+backgroundInitData : InitData
+backgroundInitData =
+    { background =
+        { backFigure = "dialogue_1"
+        , x = 0
+        , y = -300
+        }
+    , curMove = defaultCamera
+    , remainMove =
+        []
+    }

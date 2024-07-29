dd553018cc0fc94f1987485b806ac5f7f2f04033
Wang Xuanhuan <bluesirons@sjtu.edu.dn>
2024-07-29 21:42:27 +0800

    feat: dialogue

--- src/Scenes/After2/Dialogues.elm
+++ src/Scenes/After2/Dialogues.elm
@@ -85,8 +85,8 @@ ask =
     genDialogue "head_bulingze" content ( 3, 1 )
 
 
-silence : Dialogue
-silence =
+silence1 : Dialogue
+silence1 =
     let
         content =
             [ "Wenderd:"
@@ -129,6 +129,105 @@ prevent =
     genDialogue "head_bruce" content ( 3, 5 )
 
 
+apologize : Dialogue
+apologize =
+    let
+        content =
+            [ "Bruce:"
+            , "Sorry..."
+            ]
+    in
+    genDialogue "head_bruce" content ( 5, 1 )
+
+
+comfort : Dialogue
+comfort =
+    let
+        content =
+            [ "Bulingze:"
+            , "It's not your fault, Bruce."
+            ]
+    in
+    genDialogue "head_bulingze" content ( 7, 1 )
+
+
+collapse : Dialogue
+collapse =
+    let
+        content =
+            [ "Bithif:"
+            , "So is it ours?"
+            ]
+    in
+    genDialogue "head_bithif" content ( 7, 2 )
+
+
+persuade : Dialogue
+persuade =
+    let
+        content =
+            [ "Bulingze:"
+            , "...Don't talk about that. Let's go back."
+            ]
+    in
+    genDialogue "head_bulingze" content ( 7, 3 )
+
+
+hesitate : Dialogue
+hesitate =
+    let
+        content =
+            [ "Bruce:"
+            , "...We need his head."
+            ]
+    in
+    genDialogue "head_bruce" content ( 7, 4 )
+
+
+repeat : Dialogue
+repeat =
+    let
+        content =
+            [ "Bruce:"
+            , "We need his head to prove our work."
+            ]
+    in
+    genDialogue "head_bruce" content ( 7, 5 )
+
+
+astounded : Dialogue
+astounded =
+    let
+        content =
+            [ "Bithif:"
+            , "Bruce..."
+            ]
+    in
+    genDialogue "head_bithif" content ( 7, 6 )
+
+
+silence2 : Dialogue
+silence2 =
+    let
+        content =
+            [ "Bulingze:"
+            , "..."
+            ]
+    in
+    genDialogue "head_bulingze" content ( 7, 7 )
+
+
+end : Dialogue
+end =
+    let
+        content =
+            [ "Bulingze:"
+            , "Okay, let's get this over with quickly."
+            ]
+    in
+    genDialogue "head_bulingze" content ( 9, 1 )
+
+
 genDialogue : String -> List String -> ( Int, Int ) -> Dialogue
 genDialogue speaker content id =
     { defaultDialogue
@@ -142,5 +241,6 @@ dialogueInitData : InitData
 dialogueInitData =
     { curDialogue = defaultDialogue
     , remainDiaList =
-        []
+        [ 
+        ]
     }

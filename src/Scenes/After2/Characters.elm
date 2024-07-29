1307b17a8c34a7a95cf71c96505d67b73a5d25fc
Wang Xuanhuan <bluesirons@sjtu.edu.dn>
2024-07-29 19:49:06 +0800

    feat: character

---
+++ src/Scenes/After2/Characters.elm
@@ -0,0 +1,47 @@
+module Scenes.After2.Characters exposing (..)
+
+{-
+   All charater data
+-}
+
+import SceneProtos.Story.Components.CharSequence.Init exposing (Character, Direction(..), InitData, MoveKind(..), Movement, Posture(..), defaultCharacter, defaultMovement)
+
+
+characters : List Character
+characters =
+    [ genCharacter "Wenderd" Battle Right 1150 390
+    , genCharacter "Bulingze" Battle Right 1170 550
+    , genCharacter "Bruce" Battle Right 1300 350
+    , genCharacter "Bithif" Battle Right 1300 490
+    , genCharacter "Cavalry" Normal Right 540 350
+    ]
+
+
+genCharacter : String -> Posture -> Direction -> Float -> Float -> Character
+genCharacter name posture direction x y =
+    { defaultCharacter
+        | name = name
+        , posture = posture
+        , direction = direction
+        , x = x
+        , y = y
+    }
+
+
+genMove : String -> Posture -> MoveKind -> Int -> Movement
+genMove name posture movekind id =
+    { defaultMovement
+        | name = name
+        , posture = posture
+        , movekind = movekind
+        , id = id
+        , isMoving = False
+    }
+
+
+charInitData : InitData
+charInitData =
+    { characters = characters
+    , curMove = []
+    , remainMove = []
+    }

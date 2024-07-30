3ed5d12eeec26e511cb1f263a5ff4907e094d057
Wang Xuanhuan <bluesirons@sjtu.edu.dn>
2024-07-30 15:05:59 +0800

    feat: background and dialogue

---
+++ src/Scenes/After3/Dialogues.elm
@@ -0,0 +1,23 @@
+module Scenes.After3.Dialogues exposing (..)
+
+{-
+   All dialogue data
+-}
+
+import SceneProtos.Story.Components.DialogSequence.Init exposing (Dialogue, InitData, defaultDialogue)
+
+
+genDialogue : String -> List String -> ( Int, Int ) -> Dialogue
+genDialogue speaker content id =
+    { defaultDialogue
+        | speaker = speaker
+        , content = content
+        , id = id
+    }
+
+
+dialogueInitData : InitData
+dialogueInitData =
+    { curDialogue = defaultDialogue
+    , remainDiaList = []
+    }

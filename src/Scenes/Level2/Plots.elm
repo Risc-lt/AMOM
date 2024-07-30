96beea11c3c94dd037df3b90a58f0ab5adf1ac2b
Wang Xuanhuan <bluesirons@sjtu.edu.dn>
2024-07-30 20:18:26 +0800

    feat: plots

---
+++ src/Scenes/Level2/Plots.elm
@@ -0,0 +1,32 @@
+module Scenes.Level2.Plots exposing (..)
+
+{-
+   All plot data
+-}
+
+import SceneProtos.Game.Components.Dialogue.Init exposing (Dialogue, defaultDialogue)
+import SceneProtos.Game.Components.StoryTrigger.Init exposing (HealthStatus(..), TriggerConditions(..))
+
+
+genDialogue : String -> List String -> ( Int, Int ) -> Dialogue
+genDialogue speaker content id =
+    { defaultDialogue
+        | speaker = speaker
+        , content = content
+        , id = id
+    }
+
+
+dialogueInitData : { curDialogue : Dialogue, remainDiaList : List Dialogue }
+dialogueInitData =
+    { curDialogue = defaultDialogue
+    , remainDiaList =
+        [
+        ]
+    }
+
+
+triggerInitData : List ( TriggerConditions, Int )
+triggerInitData =
+    [ 
+    ]

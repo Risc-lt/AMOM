9cf5c288163cc4dd2fc37179550fb257b3ba0761
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-07-31 17:29:33 +0800

    fix: refactor Before2 scene

--- src/Lib/Resources.elm
+++ src/Lib/Resources.elm
@@ -31,13 +31,6 @@ A list of all the textures.
 
 Add your textures here. Don't worry if your list is too long. You can split those resources according to their usage.
 
-Example:
-
-    Dict.fromList
-        [ ( "ball", "assets/img/ball.png" )
-        , ( "car", "assets/img/car.jpg" )
-        ]
-
 -}
 allTexture : Dict String String
 allTexture =
@@ -95,26 +88,6 @@ bufferTexture =
 
 
 {-| Add all your sprite sheets here.
-
-Example:
-
-    allSpriteSheets =
-        Dict.fromList
-            [ ( "spritesheet1"
-              , [ ( "sp1"
-                  , { realStartPoint = ( 0, 0 )
-                    , realSize = ( 100, 100 )
-                    }
-                  )
-                , ( "sp2"
-                  , { realStartPoint = ( 100, 0 )
-                    , realSize = ( 100, 100 )
-                    }
-                  )
-                ]
-              )
-            ]
-
 -}
 allSpriteSheets : SpriteSheet
 allSpriteSheets =
---
+++ src/Scenes/Before2/Characters2.elm
@@ -0,0 +1,136 @@
+module Scenes.Before2.Characters2 exposing (charge5, charge6, charge7, charge8, charge9)
+
+{-|
+
+
+# Characters2 Module
+
+@docs charge5, charge6, charge7, charge8, charge9
+
+-}
+
+import SceneProtos.Story.Components.CharSequence.Init exposing (Direction(..), MoveKind(..), Movement, Posture(..), defaultMovement)
+
+
+genMove : String -> Posture -> MoveKind -> Int -> Movement
+genMove name posture movekind id =
+    { defaultMovement
+        | name = name
+        , posture = posture
+        , movekind = movekind
+        , id = id
+        , isMoving = False
+    }
+
+
+charge5 : List Movement
+charge5 =
+    let
+        wenderdMove =
+            Real ( 729, 400 ) 8
+
+        bulingzeMove =
+            Real ( 989, 470 ) 8
+
+        bruceMove =
+            Real ( 859, 400 ) 8
+
+        bithifMove =
+            Real ( 909, 760 ) 8
+    in
+    [ genMove "Wenderd" Battle wenderdMove 12
+    , genMove "Bulingze" Battle bulingzeMove 12
+    , genMove "Bruce" Battle bruceMove 12
+    , genMove "Bithif" Battle bithifMove 12
+    ]
+
+
+charge6 : List Movement
+charge6 =
+    let
+        wenderdMove =
+            Real ( 659, 400 ) 8
+
+        bulingzeMove =
+            Real ( 919, 470 ) 8
+
+        bruceMove =
+            Real ( 859, 330 ) 8
+
+        bithifMove =
+            Real ( 839, 760 ) 8
+    in
+    [ genMove "Wenderd" Battle wenderdMove 13
+    , genMove "Bulingze" Battle bulingzeMove 13
+    , genMove "Bruce" Battle bruceMove 13
+    , genMove "Bithif" Battle bithifMove 13
+    ]
+
+
+charge7 : List Movement
+charge7 =
+    let
+        wenderdMove =
+            Real ( 299, 400 ) 8
+
+        bulingzeMove =
+            Real ( 559, 470 ) 8
+
+        bruceMove =
+            Real ( 499, 330 ) 8
+
+        bithifMove =
+            Real ( 839, 400 ) 8
+
+        concertMove =
+            Real ( -280, 400 ) 8
+    in
+    [ genMove "Wenderd" Battle wenderdMove 14
+    , genMove "Bulingze" Battle bulingzeMove 14
+    , genMove "Bruce" Battle bruceMove 14
+    , genMove "Bithif" Battle bithifMove 14
+    , genMove "Concert" Battle concertMove 14
+    ]
+
+
+charge8 : List Movement
+charge8 =
+    let
+        move =
+            Fake Left
+
+        concertMove =
+            Follow ( 20, 400 ) 8
+    in
+    [ genMove "Wenderd" Battle move 15
+    , genMove "Bulingze" Battle move 15
+    , genMove "Bruce" Battle move 15
+    , genMove "Bithif" Battle move 15
+    , genMove "Concert" Battle concertMove 15
+    ]
+
+
+charge9 : List Movement
+charge9 =
+    let
+        wenderdMove =
+            Real ( 299, 400 ) 8
+
+        bulingzeMove =
+            Real ( 499, 470 ) 8
+
+        bruceMove =
+            Real ( 499, 330 ) 8
+
+        bithifMove =
+            Real ( 699, 400 ) 8
+
+        concertMove =
+            None Right
+    in
+    [ genMove "Wenderd" Battle wenderdMove 16
+    , genMove "Bulingze" Battle bulingzeMove 16
+    , genMove "Bruce" Battle bruceMove 16
+    , genMove "Bithif" Battle bithifMove 16
+    , genMove "Concert" Battle concertMove 16
+    ]

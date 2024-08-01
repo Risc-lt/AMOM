5b0520be801427c506671d94dfab8d339ee02b23
Wang Xuanhuan <bluesirons@sjtu.edu.dn>
2024-08-01 11:13:16 +0800

    feat: resource update

--- src/Lib/Resources.elm
+++ src/Lib/Resources.elm
@@ -66,16 +66,19 @@ chacaterTexture =
 
 chacaterSheetTexture : List ( String, String )
 chacaterSheetTexture =
-    [ ( "BulingzeSheet", "assets/character/sprite_sheet/BulingzeSheet.jpg" )
-    , ( "BruceSheet", "assets/character/sprite_sheet/BruceSheet.jpg" )
-    , ( "BithifSheet", "assets/character/sprite_sheet/BithifSheet.jpg" )
-    , ( "WenderdSheet", "assets/character/sprite_sheet/WenderdSheet.jpg" )
-    , ( "Wild WolfSheet", "assets/character/sprite_sheet/Wild WolfSheet.jpg" )
+    [ ( "BulingzeSheet", "assets/character/sprite_sheet/BulingzeSheet.png" )
+    , ( "BruceSheet", "assets/character/sprite_sheet/BruceSheet.png" )
+    , ( "BithifSheet", "assets/character/sprite_sheet/BithifSheet.png" )
+    , ( "WenderdSheet", "assets/character/sprite_sheet/WenderdSheet.png" )
+    , ( "Wild WolfSheet", "assets/character/sprite_sheet/Wild_WolfSheet.jpg" )
     , ( "SwordsmanSheet", "assets/character/sprite_sheet/SwordsmanSheet.png" )
     , ( "MagicianSheet", "assets/character/sprite_sheet/MagicianSheet.png" )
     , ( "TherapistSheet", "assets/character/sprite_sheet/TherapistSheet.png" )
     , ( "CavalrySheet", "assets/character/sprite_sheet/CavalrySheet.png" )
     , ( "ConcertSheet", "assets/character/sprite_sheet/ConcertSheet.png" )
+    , ( "ConcertFall", "assets/character/sprite_sheet/ConcertFall.png" )
+    , ( "WenderdFall", "assets/character/sprite_sheet/WenderdFall.png" )
+    , ( "Wild WolfFall", "assets/character/sprite_sheet/Wild_WolfFall.png" )
     ]
 
 

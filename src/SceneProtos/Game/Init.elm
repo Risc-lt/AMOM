f6fd830690334970857b7e99a829d20fdd7a823a
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-08-06 15:44:33 +0800

    fix: add missing docs

--- src/SceneProtos/Game/Init.elm
+++ src/SceneProtos/Game/Init.elm
@@ -1,4 +1,4 @@
-module SceneProtos.Game.Init exposing (..)
+module SceneProtos.Game.Init exposing (InitData)
 
 {-|
 
--- src/SceneProtos/Game/Play/Init.elm
+++ src/SceneProtos/Game/Play/Init.elm
@@ -14,6 +14,8 @@ import Messenger.Component.Component exposing (AbstractComponent)
 import SceneProtos.Game.Components.ComponentBase exposing (BaseData, ComponentMsg, ComponentTarget)
 
 
+{-| The data used to initialize the scene
+-}
 type alias InitData cdata scenemsg =
     { components : List (AbstractComponent cdata UserData ComponentTarget ComponentMsg BaseData scenemsg)
     }
